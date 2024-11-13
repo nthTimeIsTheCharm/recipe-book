905e990e83be9606cb15ee287c36d0ddf29a96f1
Roxana Ferramola <ferramolafvm@gmail.com>
2024-11-13 21:31:14 +0100

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-/* import Navbar from "./components/Navbar" */
+import Navbar from "./components/Navbar" 
 import Sidebar from './components/Sidebar'
 /* import Footer from './components/Footer' */
 
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-     {/* <Navbar /> */}
+      <Navbar />
      <Sidebar />
      {/* <Footer /> */}
     </>
