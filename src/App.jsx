fcd1a15c48b357402c85d18536fb195886dcf399
Eleni Kriezia <173841123+nthTimeIsTheCharm@users.noreply.github.com>
2024-11-13 21:18:05 +0100

    Cambios a sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
+/* import Navbar from "./components/Navbar" */
+import Sidebar from './components/Sidebar'
+/* import Footer from './components/Footer' */
 
 function App() {
 
   return (
     <>
-       
+     {/* <Navbar /> */}
+     <Sidebar />
+     {/* <Footer /> */}
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+import "./Sidebar.css"
+
+function Sidebar(){
+   return(
+    <ul className="sidebar">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+    </ul>
+   ); 
+}
+
+export default Sidebar;
