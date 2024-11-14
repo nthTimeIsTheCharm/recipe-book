690360c47ab2236d283d97e63db34d99d33197d3
Roxana Ferramola <ferramolafvm@gmail.com>
2024-11-14 21:59:32 +0100

    cambios de navbar y estilos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,18 @@
 import "./Navbar.css";
+import logo from "../img/logo.png"
 
 function Navbar() {
-  return <div className="navbar"></div>;
+  return (
+    <nav className="navbar">
+        <img src={logo} className="logo" ></img>
+        <h1 className="title">My recipe book</h1>
+      <ul>
+        <li><a href="#">All recipes</a></li>
+        <li><a href="#">Favorite recipes</a></li>
+        <li><a href="#">Create new recipe</a></li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
