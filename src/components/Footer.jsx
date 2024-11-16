53f4a79846f00a7e6b447d8825f72309f7faf122
Airam Santos diaz <airam@MacBook-Pro-de-Airam.local>
2024-11-16 13:24:55 +0100

    first

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,16 @@
 import "./Footer.css"
 
-function Footer (){
-
-
-    
+function Footer () {
+    return (
+        <footer className="footer">
+        <ul>
+            <li>ROXANA</li>
+            <li>ELENI</li>
+            <li>AIRAM</li>
+        </ul>
+        <a className="link" href="https://github.com/nthTimeIsTheCharm/recipe-book">Chickpea recipes Github</a>
+        </footer>
+    )
 }
-
-
 
 export default Footer
