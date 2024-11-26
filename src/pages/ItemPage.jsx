d76db53ed6273055e2443695eb9a508468a4a207
Eleni Kriezia <173841123+nthTimeIsTheCharm@users.noreply.github.com>
2024-11-26 23:47:25 +0100

    style ItemPage component

--- src/pages/ItemPage.jsx
+++ src/pages/ItemPage.jsx
@@ -1,21 +1,31 @@
 import { useParams } from "react-router-dom";
+import "./ItemPage.css";
 
+function ItemPage(props) {
+  const { recipeId } = useParams();
+  const foundRecipe = props.recipes.find((recipe) => recipe.id === recipeId);
 
-function ItemPage(props){
-const {recipeId} = useParams();
-const foundRecipe = props.recipes.find((recipe)=> recipe.id === recipeId)
+  return (
+    <div className="itemPage-recipe">
+      <div className="itemPage-info">
+        <div className="itemPage-imageWrapper">
+          <img className="itemPage-image" src={foundRecipe.image} />
+        </div>
+        <div className="itemPage-details">
+          <h1 className="itemPage-title">{foundRecipe.name}</h1>
+          <h2 className="itemPage-calories">Calories</h2>
+          <p className="itemPage-calories">{foundRecipe.calories}</p>
+          <h2 className="itemPage-servings">Servings</h2>
+          <p className="itemPage-servings">{foundRecipe.servings}</p>
+        </div>
+      </div>
 
-return(
-        <>
-        <h1>{foundRecipe.name}</h1>
-        <img src={foundRecipe.image}/>
-        <p>{foundRecipe.calories}</p>
-        <p>{foundRecipe.ingredients}</p>
-        <p>{foundRecipe.intructions}</p>
-        
-        
-        </>
-    )
+      <h2 className="itemPage-ingredients">Ingredients</h2>
+      <p className="itemPage-ingredients">{foundRecipe.ingredients}</p>
+      <h2 className="itemPage-instructions">Instructions</h2>
+      <p className="itemPage-instructions">{foundRecipe.instructions}</p>
+    </div>
+  );
 }
 
 export default ItemPage;
