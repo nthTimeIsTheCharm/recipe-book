2c35ad5faf55239b2e8dd8176e57e18c7b16f2fd
Eleni Kriezia <173841123+nthTimeIsTheCharm@users.noreply.github.com>
2024-11-29 21:25:26 +0100

    arreglar estilos tarjetas

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,27 +1,39 @@
 import "./Card.css";
-import binIcon from "../assets/delete.svg"
+import binIcon from "../assets/delete.svg";
+import heartIcon from "../assets/heart.svg"; 
 import { Link } from "react-router-dom";
 
 function Card(props) {
-
-
   return (
-    <li className="card"> 
-<Link to={"/recipe/" + props.recipe.id}>
-      <div className="image-container">
+    <li className="card">
+      <Link to={"/recipe/" + props.recipe.id}>
+        <div className="image-container">
+          <img
+            className="food-image"
+            src={props.recipe.image}
+            alt="PLACEHOLDER"
+          />
+        </div>
+        <div className="text-container">
+          <h2>{props.recipe.name}</h2>
+          <p>
+            {props.recipe.calories} calories - {props.recipe.servings} serving
+          </p>
+        </div>
+      </Link>
+      <div className="button-container">
         <img
-          className="food-image"
-          src={props.recipe.image}
-          alt="PLACEHOLDER"
+          className="delete-button"
+          src={binIcon}
+          alt="delete"
+          onClick={() => props.deleteRecipe(props.recipe.id)}
+        />
+        <img
+          className="favourite-button"
+          src={heartIcon}
+          alt="save as favourite"
         />
       </div>
-      <div className="text-container">
-        <h2>{props.recipe.name}</h2>
-        <p>{props.recipe.calories} calories</p>
-        <p>{props.recipe.servings} serving</p>
-      </div>
-      </Link>
-      <img className="delete-button" src={binIcon} alt="delete" onClick = {()=> props.deleteRecipe(props.recipe.id) } />
     </li>
   );
 }
